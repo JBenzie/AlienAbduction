615c429c43743a61d1cd4199bfedffb30ea7abb4
Jerry Benzie <gbenzie@appvion.com>
2020-11-06 10:17:59 -0500

    Added preload text and percentage

--- pub/js/titleScene.js
+++ pub/js/titleScene.js
@@ -5,6 +5,43 @@ class TitleScene extends Phaser.Scene {
 	}
 
 	preload() {
+		var width = this.game.config.width;
+		var height = this.game.config.height;
+		
+		var loadingText = this.make.text({
+			x: width / 2,
+			y: height / 2 - 50,
+			text: 'Loading...',
+			style: {
+				font: '20px monospace',
+				fill: '#ffffff'
+			}
+		});
+		loadingText.setOrigin(0.5, 0.5);
+		var percentText = this.make.text({
+			x: width / 2,
+			y: height / 2 - 5,
+			text: '0%',
+			style: {
+				font: '18px monospace',
+				fill: '#ffffff'
+			}
+		});
+		percentText.setOrigin(0.5, 0.5);
+	  
+		this.load.on('progress', function (value) {
+		  percentText.setText(parseInt(value * 100) + '%');
+		});
+	  
+		this.load.on('fileprogress', function (file) {
+	  
+		});
+	  
+		this.load.on('complete', function () {
+		  console.log('complete');
+		  loadingText.destroy();
+		  percentText.destroy();
+		});
 
 		this.load.image('sky', 'pub/assets/images/bg/parallax/7sky.png');
 		this.load.image('rocks', 'pub/assets/images/bg/parallax/6rocks.png');
